211e5211d4036054e464d64a457f0084c0161589
이해찬[Ad Creative Platform Dev] <cksgodl1@naver.com>
2025-01-10 08:23:03 +0900

    fix : cors

--- src/main/kotlin/com/neggu/neggu/config/SecurityConfig.kt
+++ src/main/kotlin/com/neggu/neggu/config/SecurityConfig.kt
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 
 @Configuration
@@ -16,6 +19,21 @@ class SecurityConfig {
             .csrf { it.disable() }
             .formLogin { it.disable() }
             .headers { it.frameOptions { it.disable() } }
+            .cors { it.configurationSource(corsConfigurationSource()) } // CORS 설정 추가
             .build()
+    }
+
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("https://neggu.site") // 허용할 Origin
+        configuration.allowedMethods = listOf("GET", "POST", "OPTIONS") // 허용할 HTTP 메서드
+        configuration.allowedHeaders = listOf("*") // 허용할 요청 헤더
+        configuration.allowCredentials = true // 인증 정보 허용
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration) // 모든 경로에 적용
+        return source
     }
 }
