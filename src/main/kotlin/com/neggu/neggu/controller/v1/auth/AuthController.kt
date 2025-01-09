1596f811719524023b42a7cae50008ad955248a6
이해찬[Ad Creative Platform Dev] <cksgodl1@naver.com>
2025-01-10 08:12:43 +0900

    test : apple oauth URL

--- src/main/kotlin/com/neggu/neggu/controller/v1/auth/AuthController.kt
+++ src/main/kotlin/com/neggu/neggu/controller/v1/auth/AuthController.kt
@@ -9,6 +9,7 @@ import com.neggu.neggu.model.auth.RegisterClaims
 import com.neggu.neggu.model.auth.OauthProvider
 import com.neggu.neggu.service.user.SocialLoginService
 import com.neggu.neggu.service.user.UserRegisterService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -36,5 +37,16 @@ class AuthController(
             OauthProvider.from(registerClaims.provider),
             userRegisterRequest,
         )
+    }
+
+    @PostMapping("/oauth/apple")
+    fun handleAppleOAuthCallback(
+        @RequestParam("code") code: String, // 인증 코드
+        @RequestParam("state", required = false) state: String?, // 상태 값 (선택적)
+        @RequestParam("id_token", required = false) idToken: String?, // ID 토큰 (선택적)
+    ): ResponseEntity<String> {
+        val result = "Apple OAuth Callback Success"
+        println("$code $state $idToken")
+        return ResponseEntity.ok(result)
     }
 }
