6425d16fbd67a465f1a9c726bc75e7f5984b11ad
이해찬[Ad Creative Platform Dev] <cksgodl1@naver.com>
2025-01-11 04:45:27 +0900

    fix : cloth Transaction

--- src/main/kotlin/com/neggu/neggu/NegguApplication.kt
+++ src/main/kotlin/com/neggu/neggu/NegguApplication.kt
@@ -3,9 +3,11 @@ package com.neggu.neggu
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableTransactionManagement(proxyTargetClass = true)
 class NegguApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/neggu/neggu/service/ClothService.kt
+++ src/main/kotlin/com/neggu/neggu/service/ClothService.kt
@@ -5,11 +5,13 @@ import com.neggu.neggu.config.LoggerConfig.nInfo
 import com.neggu.neggu.dto.cloth.ClothRegisterRequest
 import com.neggu.neggu.exception.ErrorType
 import com.neggu.neggu.exception.NotFoundException
+import com.neggu.neggu.exception.ServerException
 import com.neggu.neggu.exception.UnAuthorizedException
 import com.neggu.neggu.model.cloth.*
 import com.neggu.neggu.model.user.User
 import com.neggu.neggu.repository.BrandRepository
 import com.neggu.neggu.repository.ClothRepository
+import com.neggu.neggu.repository.UserRepository
 import com.neggu.neggu.service.aws.S3Service
 import org.bson.types.ObjectId
 import org.springframework.data.domain.Page
@@ -17,13 +19,15 @@ import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.multipart.MultipartFile
 
 @Service
 class ClothService(
     private val clothRepository: ClothRepository,
     private val brandRepository: BrandRepository,
-    private val s3Service: S3Service
+    private val s3Service: S3Service,
+    private val userRepository: UserRepository,
 ) {
 
     fun getClothPage(user: User, size:Int, page: Int, sortProperty : String = "createdAt"): Page<Cloth> {
@@ -35,6 +39,7 @@ class ClothService(
         return clothRepository.findByIdOrNull(id) ?: throw NotFoundException(ErrorType.NotFoundCloth)
     }
 
+    @Transactional
     fun postCloth(user: User, images: MultipartFile, registerRequest: ClothRegisterRequest): Cloth {
         val fileName = s3Service.uploadFile(user, images)
         val cloth = Cloth(
@@ -51,16 +56,21 @@ class ClothService(
             name = "아직 구현 중입니다 (아디다스-회색-후드집업)", // TODO: 구현 필요
             colorCode = "구현 중입니다. (#fffffff)" // TODO: 구현 필요
         )
-        return clothRepository.save(cloth).also {
-            log.nInfo("Cloth(${it.id}) saved by user ${user.id}\n Cloth Info : $it")
+
+        val savedCloth = clothRepository.save(cloth)
+        userRepository.save(user.copy(clothes = user.clothes + savedCloth.id!!))
+        return savedCloth.also {
+            log.nInfo("Save Cloth(${it.id}) saved by user ${user.id}\n Cloth Info : $it")
         }
     }
 
+    @Transactional
     fun deleteCloth(user: User, objectId: ObjectId): Cloth {
         val cloth = clothRepository.findByIdOrNull(objectId) ?: throw NotFoundException(ErrorType.NotFoundCloth)
         if (cloth.accountId != user.id) { throw UnAuthorizedException(ErrorType.InvalidIdToken) }
         clothRepository.delete(cloth)
         s3Service.deleteFile(cloth.imageUrl)
+        userRepository.save(user.copy(clothes = user.clothes.filter { it != cloth.id }))
         return cloth.also {
             log.nInfo("Cloth(${it.id}) deleted by user ${user.id}\n Cloth Info : $it")
         }
